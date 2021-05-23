01c7a5418b4e7caa2374cabf0eadc2a78905c655
i.popov <me.ilya.popov@gmail.com>
2021-05-23 21:24:24 +0300

    Added caching on AirportDistanceService level

---
+++ src/Distances.Infrastructure/Impl/Caching/CachingAirportDistanceService.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Distances.Services;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
+
+namespace Distances.Infrastructure.Impl.Caching
+{
+    public class CachingAirportDistanceService : IAirportDistanceService
+    {
+        private readonly IAirportDistanceService _inner;
+        private readonly IMemoryCache _cache;
+        private readonly IOptions<CachingAirportDistanceServiceOptions> _options;
+
+        public CachingAirportDistanceService(IAirportDistanceService inner, IMemoryCache cache,
+            IOptions<CachingAirportDistanceServiceOptions> options)
+        {
+            _inner = inner ?? throw new ArgumentNullException(nameof(inner));
+            _cache = cache ?? throw new ArgumentNullException(nameof(cache));
+            _options = options ?? throw new ArgumentNullException(nameof(options));
+        }
+
+        public async Task<Distance> GetDistance(AirportId one, AirportId two, CancellationToken ct = default)
+        {
+            var cacheKey = GetCacheKey(one, two);
+            if (_cache.TryGetValue<Distance>(cacheKey, out var distanceCached))
+                return distanceCached;
+
+            var distance = await _inner.GetDistance(one, two, ct);
+            _cache.Set(cacheKey, distance, new MemoryCacheEntryOptions
+            {
+                SlidingExpiration = _options.Value.SlidingExpiration
+            });
+
+            return distance;
+        }
+
+        private string GetCacheKey(AirportId one, AirportId two)
+        {
+            // AB distance = BA distance, so
+            var ordered = new[] {one.IataCode, two.IataCode}
+                .OrderBy(x => x);
+
+            return string.Join("-", ordered);
+        }
+    }
+
+    public class CachingAirportDistanceServiceOptions
+    {
+        public TimeSpan SlidingExpiration { get; set; } = TimeSpan.FromMinutes(1);
+    }
+}
--- src/Distances/Startup.cs
+++ src/Distances/Startup.cs
@@ -1,11 +1,14 @@
 using Distances.Filters;
 using Distances.Infrastructure.Impl;
+using Distances.Infrastructure.Impl.Caching;
 using Distances.Infrastructure.Impl.CTeleport;
 using Distances.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using RestSharp;
 
 namespace Distances
@@ -22,10 +25,16 @@ namespace Distances
                 .AddControllersAsServices();
 
             services.AddSingleton<ILocationDistanceService, LocationDistanceService>();
-            services.AddSingleton<IAirportDistanceService, AirportDistanceService>();
+
+            services.AddSingleton<AirportDistanceService>();
             services.AddSingleton<ICTeleportClient>(
                 new CTeleportClient(
                     new RestClient("https://places-dev.cteleport.com")));
+
+            services.AddMemoryCache();
+            services.AddSingleton<IAirportDistanceService>(provider => new CachingAirportDistanceService(
+                provider.GetRequiredService<AirportDistanceService>(), provider.GetRequiredService<IMemoryCache>(),
+                provider.GetRequiredService<IOptions<CachingAirportDistanceServiceOptions>>()));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
